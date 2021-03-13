9e0d7e64358e6809aa1fd6f6ffa0bb4d89cdd73d
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-13 16:26:39 +0100

    add sorter for Characters example

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const charactersCollection = new CharactersCollection('adrianxyz');
-const sorter = new Sorter(numbersCollection);
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorterNums = new Sorter(numbersCollection);
+const sorterChars = new Sorter(charactersCollection);
+sorterChars.sort();
+sorterNums.sort();
 console.log(charactersCollection.data);
